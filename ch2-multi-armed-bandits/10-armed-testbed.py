fae50d3ee4ca4813df52308ed80f54c39840d311
devsjc <dev.solc@outlook.com>
2022-06-12 15:21:16 +0100

    Made less oop-based

--- ch2-multi-armed-bandits/10-armed-testbed.py
+++ ch2-multi-armed-bandits/10-armed-testbed.py
@@ -1,76 +1,112 @@
 """
 Assessing the effectiveness of greedy and ε-greedy action value methods.
 
-Creates a testbed consisting of a set of 2000 k-armed bandits with k=10. Each bandit problem has action values selected
-from a gaussian distribution of mean 0 and variance 1. Then an agent is run for 1000 steps on each problem, and its
-learning behaviour analyzed.
+Creates a testbed consisting of a set of 2000 k-armed bandits with k=10. Each bandit problem has action values q*
+selected from a gaussian distribution of mean 0 and variance 1. Then an agent is run for 1000 steps on each problem,
+and its learning behaviour analyzed.
 """
 
-from typing import Dict, List
+import random
+
+from typing import List, Tuple, NamedTuple
 import numpy as np
 import matplotlib.pyplot as plt
 
+# --- Define constants for the testbed
+K = 10  # number of arms on bandit
+STEPS = 1000  # number of learning steps taken per problem run
+PROBLEMS = 2000  # number of bandit problems to be solved
+DEFAULT_ACTION_VALUE_ESTIMATE = 0  # default estimated action value
 
+
+# ActionValue holds all the data required to estimate the value of
+# an action using the sample average method
+class ActionValue(NamedTuple):
+    sum: float
+    num: int
+
+    # estimate_value gets an estimation for the expected reward received
+    # from carrying out the action, using the sample-average method:
+    # the mean of all the rewards previously received for that action
+    def estimate_value(self) -> float:
+        return self.sum / self.num if self.num > 0 else DEFAULT_ACTION_VALUE_ESTIMATE
+
+
+# Bandit creates an instance of a bandit with K arms, each with true
+# action values selected from a normal distribution
 class Bandit:
-    def __init__(self, k: int):
-        self.k = k
-        self.true_action_values = np.random.normal(size=(k,))
+    def __init__(self):
+        self.true_action_values: List = np.random.normal(size=(K,)).tolist()
 
+    # reward Rt for an action is selected from a normal distribution
+    # with mean q* of that action and variance 1
     def get_reward(self, action_number: int) -> float:
-        if action_number > self.k:
-            raise Exception(f"Can't access action with number {action_number} for a {self.k} armed bandit")
+        if action_number >= K:
+            raise Exception(f"Can't access action with number {action_number} for a {K} armed bandit")
         return np.random.normal(loc=self.true_action_values[action_number])
 
-    def get_optimal_action(self) -> int:
-        return np.max(self.true_action_values)
+
+# select_action defines our action selection rule. In this case ε-greedy.
+def select_action(epsilon: float, action_values: List[ActionValue]) -> int:
+    # take exploratory step with probability epsilon
+    if np.random.normal() <= epsilon:
+        return np.random.randint(K)
+    # otherwise, take the action with the highest estimated value
+    else:
+        action_value_estimates = [av.estimate_value() for av in action_values]
+        max_value_actions = np.argwhere(action_value_estimates == np.amax(action_value_estimates)).flatten().tolist()
+        # if there is more than one highest value, select one at random
+        greedy_action = random.choice(max_value_actions)
+        return greedy_action
 
 
-class Agent:
-    def __init__(self, epsilon: float, bandit: Bandit):
-        self.epsilon: float = epsilon
-        self.bandit: Bandit = bandit
-        # Initialise all action estimates as 0
-        self.action_value_estimates: [] = np.zeros(shape=(self.bandit.k,))
-        # Create memory for storing received rewards
-        received_rewards = {}
-        [received_rewards.setdefault(i, []) for i in range(self.bandit.k)]
-        self.received_rewards: Dict[int, List] = received_rewards
-        self.average_rewards: [] = []
+# update_action_value_estimates keeps track of the sum of our rewards and the number of
+# times each reward has been chosen
+def update_action_value_estimates(action_value_estimates: List[ActionValue], reward_value: float,
+                                  action_number: int) -> List[ActionValue]:
+    new_estimates = [av if i != action_number
+                     else ActionValue(av.sum + reward_value, av.num + 1)
+                     for i, av in enumerate(action_value_estimates)]
+    return new_estimates
 
-    def take_step(self):
-        # Take exploratory step with probability epsilon
-        if np.random.normal() <= self.epsilon:
-            action_number = np.random.randint(10)
-        # Otherwise, take the action with the highest estimated value
-        else:
-            action_number = np.argmax(self.action_value_estimates)
 
-        # Update probability for that action
-        self.received_rewards[action_number].append(self.bandit.get_reward(action_number))
-        self.action_value_estimates[action_number] = np.mean(self.received_rewards[action_number])
+# take_step carries out a single step of the learning process
+def take_step(bandit: Bandit, epsilon: float, action_value_estimates: List[ActionValue]) \
+        -> Tuple[List[ActionValue], float]:
+    # choose an action according to our action selection method
+    action_number = select_action(epsilon, action_value_estimates)
+    # get a reward according to our reward distribution
+    attained_reward = bandit.get_reward(action_number)
+    # keep track of the attained rewards and update action value estimates
+    new_av_estimates = update_action_value_estimates(action_value_estimates, attained_reward, action_number)
 
-    def update_average_rewards(self):
-        self.average_rewards.append(np.mean(np.concatenate(list(self.received_rewards.values()))))
-
-    def run(self, steps: int):
-        for i in range(steps):
-            self.take_step()
-            self.update_average_rewards()
+    return new_av_estimates, attained_reward
 
 
 def main():
-    num_steps = 1000
-    num_problems = 2000
 
-    rewards = np.empty(shape=(num_problems, num_steps))
-    for i in range(num_problems):
-        agent = Agent(epsilon=0.1, bandit=Bandit(k=10))
-        agent.run(steps=num_steps)
-        rewards[i] = agent.average_rewards
+    for epsilon in [0, 0.01, 0.1]:
+        sum_rewards = [0 for _ in range(STEPS)]
+        # loop over all the bandit problems
+        for problem in range(PROBLEMS):
+            action_value_estimates: List[ActionValue] = [ActionValue(0, 0) for _ in range(K)]
+            bandit = Bandit()
+            # perform a run of STEPS steps on the current bandit problem
+            # update the sum_rewards list by adding to it the reward
+            # attained at each step
+            for step in range(STEPS):
+                action_value_estimates, received_reward = take_step(
+                    bandit=bandit,
+                    epsilon=epsilon,
+                    action_value_estimates=action_value_estimates)
 
-    avg_rewards = np.mean(rewards, axis=0)
+                sum_rewards[step] = sum_rewards[step] + received_reward
 
-    plt.scatter(x=np.arange(start=1, stop=len(avg_rewards) + 1), y=avg_rewards)
+        # plot each epsilon value as a line graph
+        avg_rewards = [sum_rewards[i] / PROBLEMS for i in range(STEPS)]
+        plt.plot([i for i in range(STEPS)], avg_rewards, label=epsilon.__str__())
+
+    plt.legend()
     plt.show()
 
 
